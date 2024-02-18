36fa11695dabf146e9d6b8ecb1dc83e9bb149faf
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2024-02-18 14:48:10 +0800

    Remove firebase credentials

--- backend/firebase_setup/firebase_config.py
+++ backend/firebase_setup/firebase_config.py
@@ -2,6 +2,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase
-cred = credentials.Certificate("backend/firebase_setup/key.json")
+cred = credentials.Certificate("")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
